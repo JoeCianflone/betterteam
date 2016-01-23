848b4fc7c02d6cf043e039e36d4f33dfcbc38a50
Joe Cianflone <joe@cianflone.co>
2016-01-23 15:17:50 -0500

    initial commit of framework, footer and header

---
+++ src/js/modules/introduction.js
@@ -0,0 +1,15 @@
+App.Modules = App.Modules || {};
+App.Modules.Introduction = function () {
+   var o = { };
+
+   return {
+      init: function() {
+         return this;
+      },
+      events: function() {
+         return this;
+      }
+   };
+
+}();
+
